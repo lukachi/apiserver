2d0bdf4aad302778e9af43ffe8fefc30482d916e
lukachi <lukachisama@gmail.com>
2021-02-23 10:47:35 +0200

    finish

--- internal/app/apiserver/server_internal_test.go
+++ internal/app/apiserver/server_internal_test.go
@@ -3,6 +3,8 @@ package apiserver
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
+	"github.com/gorilla/securecookie"
 	"github.com/gorilla/sessions"
 	"github.com/lukachi/apiserver/internal/app/model"
 	"github.com/lukachi/apiserver/internal/app/store/teststore"
@@ -11,6 +13,49 @@ import (
 	"net/http/httptest"
 	"testing"
 )
+
+func TestServer_AuthenticateUser(t *testing.T) {
+	store := teststore.New()
+	u := model.TestUser(t)
+	store.User().Create(u)
+
+	testCases := []struct {
+		name         string
+		cookieValue  map[interface{}]interface{}
+		expectedCode int
+	}{
+		{
+			name: "authenticated",
+			cookieValue: map[interface{}]interface{}{
+				"user_id": u.ID,
+			},
+			expectedCode: http.StatusOK,
+		},
+		{
+			name: "not authenticated",
+			cookieValue: nil,
+			expectedCode: http.StatusUnauthorized,
+		},
+	}
+
+	secretKey := "secret"
+	s := NewServer(store, sessions.NewCookieStore([]byte(secretKey)))
+	sc := securecookie.New([]byte(secretKey), nil)
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			rec := httptest.NewRecorder()
+			req, _ := http.NewRequest(http.MethodGet, "/", nil)
+			cookieStr, _ := sc.Encode(sessionName, tc.cookieValue)
+			req.Header.Set("Cookie", fmt.Sprintf("%s=%s", sessionName, cookieStr))
+			s.authenticateUser(handler).ServeHTTP(rec, req)
+			assert.Equal(t, tc.expectedCode, rec.Code)
+		})
+	}
+}
 
 func TestServer_HandleUsersCreate(t *testing.T) {
 	s := NewServer(teststore.New(), sessions.NewCookieStore([]byte("secret")))
@@ -66,21 +111,21 @@ func TestServer_HandleSessionsCreate(t *testing.T) {
 	}{
 		{
 			name: "valid",
-			payload: map[string]string {
-				"email": u.Email,
+			payload: map[string]string{
+				"email":    u.Email,
 				"password": u.Password,
 			},
 			expectedCode: http.StatusOK,
 		},
 		{
-			name: "invalid payload",
-			payload: "invalid",
+			name:         "invalid payload",
+			payload:      "invalid",
 			expectedCode: http.StatusBadRequest,
 		},
 		{
 			name: "invalid email",
-			payload: map[string]string {
-				"email": "invalid",
+			payload: map[string]string{
+				"email":    "invalid",
 				"password": u.Password,
 			},
 			expectedCode: http.StatusUnauthorized,
